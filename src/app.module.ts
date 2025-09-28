b5a3918fc0943666f75d77cc2086a3e5890b8cd3
swajan-75 <swajanbarua09@gmail.com>
2025-09-28 11:09:12 +0600

    update database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,31 +1,37 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { GoogleSheetsService } from './services/google-sheets.service';
-import { TelegramService } from './services/telegram.service';
-import { SmsController } from './sms/sms.controller';
-import { BalanceService } from './services/balance.service';
+// app.module.ts
+import { Module, OnModuleInit } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { SmsModule } from './sms/sms.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     UserModule,
     SmsModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5433,
-      username: '',
-      password: '',
-      database: 'bkash_p2p',
-      autoLoadEntities: true,
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const ca = configService.get<string>('DB_CA_CERT');
+        //console.log('CA Certificate:\n', ca ? ca.replace(/\\n/g, '\n') : 'No CA certificate found'); // <-- log here
+        return {
+          type: 'postgres',
+          host: configService.get<string>('DB_HOST'),
+          port: parseInt(configService.get<string>('DB_PORT') || '15138'),
+          username: configService.get<string>('DB_USER'),
+          password: configService.get<string>('DB_PASS'),
+          database: configService.get<string>('DB_NAME'),
+          autoLoadEntities: true,
+          synchronize: true,
+          ssl: {
+            rejectUnauthorized: true,
+            ca: ca ? ca.replace(/\\n/g, '\n') : undefined,
+          },
+        };
+      },
     }),
-    
   ],
-  controllers: [AppController, SmsController],
-  providers: [AppService, GoogleSheetsService, TelegramService , BalanceService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import * as bodyParser from 'body-parser';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
   app.enableCors();
